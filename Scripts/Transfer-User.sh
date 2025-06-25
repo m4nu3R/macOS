d744d579051a7959a15a3aa870fab7b3d5545bb2
m4nu3R <manuel.rangel.es@gmail.com>
2025-06-25 12:12:22 +0200

    Script ejecutable para mover archivos
    Este script sirve para mover archivos entre usuarios locales en un dispositivo macOS, la finalidad es facilitar el traslado de los archivos.
    
    Signed-off-by: m4nu3R <manuel.rangel.es@gmail.com>

---
+++ Scripts/Transfer-User.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+#Archivo creado por Manuel Rangel para COX bajo un acuerdo de servicios con Decskill
+
+
+# Revisión de estado de usuarios que no estén iniciados y tampoco archivos en uso.
+
+#Determinar usuario con la sesión activa.
+usuario="$(who|awk '/console/ {print $1}')"
+usuarios="$(dscl . list /Users | grep -v '_')"
+
+
+#Revisiones
+
+#Revisa que si está en el dominio.
+check4AD=$(/usr/bin/dscl localhost -list . | grep "Active Directory")
+if [ "${check4AD}" != "Active Directory" ]; then
+    echo "Este ordenador no está añadido al dominio, revisa el estado. "
+    exit 1
+fi
+
+#Mensajes iniciales para confirmación del proceso
+echo "El usuario iniciado es": $usuario
+echo "Los usuarios registrados en el ordenador son:" $usuarios
+echo "Ingrese el usuario origen de la información:" $origen
+read origen
+echo "Ingrese el usuario destino o a donde se transferirá la información:" $destino
+read destino
+
+#Determinar si está iniciada la sesión
+if [ "$usuario" = "$origen" ]; then
+    echo "$origen con sesión iniciada. Deteniendo"
+    echo "Este ordenador está iniciado con el usuario $origen, revisa si es correcto. "
+    exit 1
+else
+    if [ "$usuario" = "$destino" ]; then
+    echo "$destino con sesión iniciada. Deteniendo"
+    echo "Este ordenador está iniciado con el usuario $destino, revisa si es correcto. "
+    exit 1
+else
+echo "Ningun usuario con sesión iniciada, continuar."
+    #exit 0
+    fi
+fi
+
+#Determinar si existen realmente los usuarios
+if [ -d "/Users/$origen" ]; then 
+result="/Users/$origen existe" echo "<result>$result</result>"
+else echo "<result>/Users/$origen no existe. Deteniendo.</result>"
+exit 1
+fi
+if [ -d "/Users/$destino" ]; then 
+result="/Users/$destino existe" echo "<result>$result</result>"
+else echo "<result>/Users/$destino no existe. Deteniendo.</result>"
+exit 1
+fi
+
+# Renombrar Directorio Destino
+mv /Users/$destino /Users/$destino-antes
+echo "Renombrado /Users/$destino a /Users/$destino-antes"
+# Renombrar directorio de origen
+mv /Users/$origen /Users/$destino
+echo "Renombrado /Users/$origen to /Users/$destino"
+# Cambiar permisos de propiedad en el destino
+chown -R $destino /Users/$destino
+echo "Cambiada la propiedad de los archivos transferidos de $origen a $destino"
+exit 0
